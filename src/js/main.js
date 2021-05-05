e9de10bd74ba2f7820275962488e0d564e119b90
lock <falsivates@gmail.com>
2021-05-05 17:59:11 +0300

    commit

---
+++ src/js/main.js
@@ -0,0 +1,132 @@
+$(document).ready(function () {
+
+    $(function () {
+        $('ul.tabs__caption').on('click', 'li:not(.active)', function () {
+            $(this)
+                .addClass('active').siblings().removeClass('active')
+                .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+        });
+    });
+
+    $('.slick-objects').slick({
+        slidesToShow: 2,
+        infinite: true,
+        prevArrow: $('.slick-objects__prev'),
+        nextArrow: $('.slick-objects__next'),
+    })
+
+    const asides = document.querySelectorAll('.objects__aside');
+    $('.slick-objects').on('afterChange', function (event, slick, currentSlide) {
+        asides.forEach((aside, i) => {
+            aside.classList.remove('active');
+            if (currentSlide == i) {
+                aside.classList.add('active');
+            }
+        })
+    });
+
+    $(function () {
+        let $slider = $('.slick-hero');
+
+        if ($slider.length) {
+            let currentSlide;
+            let slidesCount;
+            let sliderCounter = document.createElement('div');
+            sliderCounter.classList.add('slider__counter');
+
+            let updateSliderCounter = function (slick, currentIndex) {
+                currentSlide = slick.slickCurrentSlide() + 1;
+                slidesCount = slick.slideCount;
+                $(sliderCounter).html(currentSlide + '<span class="line"></span>' + slidesCount)
+            };
+
+            $slider.on('init', function (event, slick) {
+                $slider.prepend(sliderCounter);
+                updateSliderCounter(slick);
+            });
+
+            $slider.on('afterChange', function (event, slick, currentSlide) {
+                updateSliderCounter(slick, currentSlide);
+            });
+
+            $slider.slick({
+                slidesToShow: 1,
+                infinite: false,
+                prevArrow: $('.slick-hero__prev'),
+                nextArrow: $('.slick-hero__next'),
+            });
+        }
+    });
+
+    $(function () {
+        let $sliderAdvantage = $('.slick-advantage');
+
+        if ($sliderAdvantage.length) {
+            let currentSlide;
+            let slidesCount;
+            let sliderCounter = document.createElement('div');
+            sliderCounter.classList.add('slider__counter');
+            sliderCounter.classList.add('slider__counter--blue');
+
+            let updateSliderCounter = function (slick, currentIndex) {
+                currentSlide = slick.slickCurrentSlide() + 1;
+                slidesCount = slick.slideCount;
+                $(sliderCounter).html(currentSlide + '<span class="line line--blue"></span>' + slidesCount)
+            };
+
+            $sliderAdvantage.on('init', function (event, slick) {
+                $sliderAdvantage.prepend(sliderCounter);
+                updateSliderCounter(slick);
+            });
+
+            $sliderAdvantage.on('afterChange', function (event, slick, currentSlide) {
+                updateSliderCounter(slick, currentSlide);
+            });
+
+            $sliderAdvantage.slick({
+                slidesToShow: 4,
+                infinite: true,
+                prevArrow: $('.slick-advantage__prev'),
+                nextArrow: $('.slick-advantage__next'),
+            });
+        }
+    });
+
+    // function slickObjects() {
+    //     const asides = document.querySelectorAll('.objects__aside');
+    //     const arrowsObject = document.querySelectorAll('.slick-objects__arrow');
+    //     const itemsObject = document.querySelectorAll('.slick-objects__item');
+    //     // const countItem = document.querySelectorAll('[data-slick-index]');
+
+    //     function hideAsides() {
+    //         asides.forEach(item => {
+    //             item.classList.remove('active');
+    //         })
+    //         asides[0].classList.add('active');
+    //     }
+    //     hideAsides();
+
+    //     arrowsObject.forEach(arrow => {
+    //         arrow.addEventListener('click', function () {
+    //             asides.forEach(aside => {
+    //                 aside.classList.remove('active');
+    //             });
+    //             itemsObject.forEach((item, i) => {
+    //                 if (item[i].getAttribute('data-slick-index') == i) {
+    //                     asides[i].classList.add('active');
+    //                 }
+    //                 // if (itemsObject[countItem.getAttribute('data-slick-index')].classList.contains('slick-current')) {
+    //                 //     asides[countItem.getAttribute('data-slick-index')].classList.add('active');
+    //                 // }
+    //             })
+    //         })
+    //     })
+    // }
+
+    // slickObjects();
+
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+
+})
