bbc1d28d48eda5c1a5a1f6fcfe5c91dbad2e1f86
lock <falsivates@gmail.com>
2021-05-21 17:00:49 +0300

    commit

--- src/js/select-water.js
+++ src/js/select-water.js
@@ -1,3 +1,10 @@
-document.addEventListener('DOMContetLoaded', function() {
-    
+$(document).ready(function () {
+    let lengthOptions = $('#type-water option').length;
+    $('#type-water').change(function () {
+        for (let i = 0; i <= lengthOptions; i++) {
+            if ($('#type-water').val() == i) {
+                console.log('Значение' + i);
+            }
+        }
+    })
 })
