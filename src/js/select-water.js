12ba266147eec57ebdeb84ed6c8c74ac814e45c9
lock <falsivates@gmail.com>
2021-05-28 15:36:41 +0300

    commit

--- src/js/select-water.js
+++ src/js/select-water.js
@@ -1,9 +1,14 @@
 $(document).ready(function () {
-    let lengthOptions = $('#type-water option').length;
+    const coefficient = [1, 2, 3, 4, 5];
+    const inputCoefficient = document.querySelector('#coefficient');
+    const lengthOptions = $('#type-water option').length;
+
+    inputCoefficient.value = coefficient[0];
+
     $('#type-water').change(function () {
         for (let i = 0; i <= lengthOptions; i++) {
             if ($('#type-water').val() == i) {
-                console.log('Значение' + i);
+                inputCoefficient.value = coefficient[i];
             }
         }
     })
