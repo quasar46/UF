0d1d6986e8e3fe249a259f777db8b286a5c42585
lock <falsivates@gmail.com>
2021-06-04 14:52:57 +0300

    commit

--- src/js/change-device.js
+++ src/js/change-device.js
@@ -69,4 +69,106 @@ document.addEventListener('DOMContentLoaded', function () {
         })
     }
     showDeviceMobile();
+
+    const showPreviewDesktop = function () {
+        const wrapperMiniDesktop = document.querySelector('#deviceMiniDesktop');
+        const wrapperNormaDesktop = document.querySelector('#deviceNormaDesktop');
+        const wrapperPromDesktop = document.querySelector('#devicePromDesktop');
+
+        const tabsPreviewMiniDesktop = wrapperMiniDesktop.querySelectorAll('.device__review-tab ul li');
+        const previewsMiniDesktop = wrapperMiniDesktop.querySelectorAll('.tabs__content');
+        const tabsPreviewNormaDesktop = wrapperNormaDesktop.querySelectorAll('.device__review-tab ul li');
+        const previewsNormaDesktop = wrapperNormaDesktop.querySelectorAll('.tabs__content');
+        const tabsPreviewPromDesktop = wrapperPromDesktop.querySelectorAll('.device__review-tab ul li');
+        const previewsPromDesktop = wrapperPromDesktop.querySelectorAll('.tabs__content');
+
+        tabsPreviewMiniDesktop.forEach((tab, i) => {
+            tab.addEventListener('click', () => {
+                tabsPreviewMiniDesktop.forEach(item => {
+                    item.classList.remove('active');
+                })
+                tab.classList.add('active');
+                previewsMiniDesktop.forEach(item => {
+                    item.classList.remove('active');
+                })
+                previewsMiniDesktop[i].classList.add('active');
+            })
+        })
+        tabsPreviewNormaDesktop.forEach((tab, i) => {
+            tab.addEventListener('click', () => {
+                tabsPreviewNormaDesktop.forEach(item => {
+                    item.classList.remove('active');
+                })
+                tab.classList.add('active');
+                previewsNormaDesktop.forEach(item => {
+                    item.classList.remove('active');
+                })
+                previewsNormaDesktop[i].classList.add('active');
+            })
+        })
+        tabsPreviewPromDesktop.forEach((tab, i) => {
+            tab.addEventListener('click', () => {
+                tabsPreviewPromDesktop.forEach(item => {
+                    item.classList.remove('active');
+                })
+                tab.classList.add('active');
+                previewsPromDesktop.forEach(item => {
+                    item.classList.remove('active');
+                })
+                previewsPromDesktop[i].classList.add('active');
+            })
+        })
+    }
+    showPreviewDesktop();
+
+    const showPreviewMobile = function () {
+        const wrapperMiniMobile = document.querySelector('#deviceMiniMobile');
+        const wrapperNormaMobile = document.querySelector('#deviceNormaMobile');
+        const wrapperPromMobile = document.querySelector('#devicePromMobile');
+
+        const tabsPreviewMiniMobile = wrapperMiniMobile.querySelectorAll('.device__review-tab ul li');
+        const previewsMiniMobile = wrapperMiniMobile.querySelectorAll('.tabs__content');
+        const tabsPreviewNormaMobile = wrapperNormaMobile.querySelectorAll('.device__review-tab ul li');
+        const previewsNormaMobile = wrapperNormaMobile.querySelectorAll('.tabs__content');
+        const tabsPreviewPromMobile = wrapperPromMobile.querySelectorAll('.device__review-tab ul li');
+        const previewsPromMobile = wrapperPromMobile.querySelectorAll('.tabs__content');
+
+        tabsPreviewMiniMobile.forEach((tab, i) => {
+            tab.addEventListener('click', () => {
+                tabsPreviewMiniMobile.forEach(item => {
+                    item.classList.remove('active');
+                })
+                tab.classList.add('active');
+                previewsMiniMobile.forEach(item => {
+                    item.classList.remove('active');
+                })
+                previewsMiniMobile[i].classList.add('active');
+            })
+        })
+        tabsPreviewNormaMobile.forEach((tab, i) => {
+            tab.addEventListener('click', () => {
+                tabsPreviewNormaMobile.forEach(item => {
+                    item.classList.remove('active');
+                })
+                tab.classList.add('active');
+                previewsNormaMobile.forEach(item => {
+                    item.classList.remove('active');
+                })
+                previewsNormaMobile[i].classList.add('active');
+            })
+        })
+        tabsPreviewPromMobile.forEach((tab, i) => {
+            tab.addEventListener('click', () => {
+                tabsPreviewPromMobile.forEach(item => {
+                    item.classList.remove('active');
+                })
+                tab.classList.add('active');
+                previewsPromMobile.forEach(item => {
+                    item.classList.remove('active');
+                })
+                previewsPromMobile[i].classList.add('active');
+            })
+        })
+    }
+    showPreviewMobile();
 })
--- src/js/select-water.js
+++ src/js/select-water.js
@@ -1,15 +1,51 @@
 $(document).ready(function () {
-    const coefficient = [1, 2, 3, 4, 5];
-    const inputCoefficient = document.querySelector('#coefficient');
-    const lengthOptions = $('#type-water option').length;
+    const checkboxes = document.querySelectorAll('.check-water');
+    const blocksInput = document.querySelectorAll('.change__inputs');
+    const coefficient1 = [1, 2, 3, 4, 5];
+    const coefficient2 = [6, 7, 8, 9, 10];
+    const inputCoefficient1 = document.querySelector('.coefficient1');
+    const inputCoefficient2 = document.querySelector('.coefficient2');
+    const optionType1 = document.querySelectorAll('.options-type-1 option');
+    const optionType2 = document.querySelectorAll('.options-type-2 option');
+    const options = document.querySelectorAll('.type-water option');
+    const lengthOptions = $('.type-water option').length;
+    const typeWaters = document.querySelectorAll('.type-water');
 
-    inputCoefficient.value = coefficient[0];
+    checkboxes[0].checked = true;
+    inputCoefficient1.value = coefficient1[0];
+    inputCoefficient2.value = coefficient2[0];
 
-    $('#type-water').change(function () {
-        for (let i = 0; i <= lengthOptions; i++) {
-            if ($('#type-water').val() == i) {
-                inputCoefficient.value = coefficient[i];
-            }
-        }
+    checkboxes.forEach((check, i) => {
+        check.addEventListener('change', () => {
+            inputCoefficient1.value = coefficient1[0];
+            inputCoefficient2.value = coefficient2[0];
+            optionType1[0].selected = true;
+            optionType2[0].selected = true;
+            blocksInput.forEach(block => {
+                block.classList.remove('active');
+            })
+            blocksInput[i].classList.add('active');
+        })
     })
+
+    typeWaters.forEach(item => {
+        item.addEventListener('change', () => {
+            for (let i = 0; i <= lengthOptions; i++) {
+                if ($(item).val() == i) {
+                    inputCoefficient1.value = coefficient1[i];
+                }
+            }
+        })
+    })
+    typeWaters.forEach(item => {
+        item.addEventListener('change', () => {
+            for (let i = 0; i <= lengthOptions; i++) {
+                if ($(item).val() == i) {
+                    inputCoefficient2.value = coefficient2[i];
+                }
+            }
+        })
+    })
+
+
 })
